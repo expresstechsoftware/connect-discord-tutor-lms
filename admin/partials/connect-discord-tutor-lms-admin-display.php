c0477cf4629234de0b1369698aeb2e2aa0772a69
Younes DRO <dro66_8@hotmail.fr>
2023-01-21 11:07:13 +0100

    Add Application details form

--- admin/partials/connect-discord-tutor-lms-admin-display.php
+++ admin/partials/connect-discord-tutor-lms-admin-display.php
@@ -12,5 +12,28 @@
  * @subpackage Connect_Discord_Tutor_Lms/admin/partials
  */
 ?>
+<?php
+if ( isset( $_GET['save_settings_msg'] ) ) {
+	?>
+	<div class="notice notice-success is-dismissible support-success-msg">
+		<p><?php echo esc_html( $_GET['save_settings_msg'] ); ?></p>
+	</div>
+	<?php
+}
+?>
+<h1><?php esc_html_e( 'TUTOR LMS Discord Add On Settings', 'connect-discord-tutor-lms' ); ?></h1>
+		<div id="tutor-lms-discord-outer" class="skltbs-theme-light" data-skeletabs='{ "startIndex": 0 }'>
+			<ul class="skltbs-tab-group">
+				<li class="skltbs-tab-item">
+				<button class="skltbs-tab" data-identity="settings" ><?php esc_html_e( 'Application Details', 'connect-discord-tutor-lms' ); ?><span class="initialtab spinner"></span></button>
+				</li>				                            
+			</ul>
+			<div class="skltbs-panel-group">
+				<div id="ets_tutor_lms_application_details" class="tutor-lms-discord-tab-conetent skltbs-panel">
+				<?php require_once CONNECT_DISCORD_TUTOR_LMS_PLUGIN_DIR_PATH . 'admin/partials/pages/connect-discord-tutor-lms-application-details.php'; ?>
+				</div>
+											
+			</div>  
+		</div>
 
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
