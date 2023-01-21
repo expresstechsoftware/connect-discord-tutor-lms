92b7a16cdf7e3c30ed7ce78c239234cb54342491
Younes DRO <dro66_8@hotmail.fr>
2023-01-21 11:08:23 +0100

    Add enqueue scripts and style

--- admin/class-connect-discord-tutor-lms-admin.php
+++ admin/class-connect-discord-tutor-lms-admin.php
@@ -72,7 +72,9 @@ class Connect_Discord_Tutor_Lms_Admin {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-
+		$min_css = ( defined( 'WP_DEBUG' ) && true === WP_DEBUG ) ? '' : '.min';
+		wp_register_style( $this->plugin_name . '-select2', plugin_dir_url( __FILE__ ) . 'css/select2.css', array(), $this->version, 'all' );
+		wp_register_style( $this->plugin_name . 'discord_tabs_css', plugin_dir_url( __FILE__ ) . 'css/skeletabs.css', array(), $this->version, 'all' );
 		wp_register_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/connect-discord-tutor-lms-admin.css', array(), $this->version, 'all' );
 
 	}
@@ -95,8 +97,17 @@ class Connect_Discord_Tutor_Lms_Admin {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
+		wp_register_script( $this->plugin_name . '-select2', plugin_dir_url( __FILE__ ) . 'js/select2.js', array( 'jquery' ), $this->version, false );
 
+		wp_register_script( $this->plugin_name . '-tabs-js', plugin_dir_url( __FILE__ ) . 'js/skeletabs.js', array( 'jquery' ), $this->version, false );
 		wp_register_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/connect-discord-tutor-lms-admin.js', array( 'jquery' ), $this->version, false );
+		$script_params = array(
+			'admin_ajax'                  => admin_url( 'admin-ajax.php' ),
+			'permissions_const'           => CONNECT_DISCORD_TUTOR_LMS_OAUTH_SCOPES,
+			'is_admin'                    => is_admin(),
+			'ets_tutor_lms_discord_nonce' => wp_create_nonce( 'ets-tutor-lms-discord-ajax-nonce' ),
+		);
+		wp_localize_script( $this->plugin_name, 'etsTutorLms', $script_params );
 
 	}
 
@@ -120,8 +131,47 @@ class Connect_Discord_Tutor_Lms_Admin {
 			wp_send_json_error( 'You do not have sufficient rights 1', 403 );
 			exit();
 		}
+
+		wp_enqueue_style( $this->plugin_name . '-select2' );
+		wp_enqueue_style( $this->plugin_name . 'discord_tabs_css' );
 		wp_enqueue_style( $this->plugin_name );
+		wp_enqueue_script( $this->plugin_name . '-select2' );
+		wp_enqueue_script( $this->plugin_name . '-tabs-js' );
 		wp_enqueue_script( $this->plugin_name );
+
+		require_once CONNECT_DISCORD_TUTOR_LMS_PLUGIN_DIR_PATH . 'admin/partials/connect-discord-tutor-lms-admin-display.php';
+
+	}
+	/**
+	 * Update redirect url
+	 *
+	 * @param NONE
+	 * @return NONE
+	 */
+	public function ets_tutor_lms_discord_update_redirect_url() {
+
+		if ( ! current_user_can( 'administrator' ) ) {
+			wp_send_json_error( 'You do not have sufficient rights', 403 );
+			exit();
+		}
+		// Check for nonce security
+		if ( ! wp_verify_nonce( $_POST['ets_tutor_lms_discord_nonce'], 'ets-tutor-lms-discord-ajax-nonce' ) ) {
+			wp_send_json_error( 'You do not have sufficient rights', 403 );
+			exit();
+		}
+
+		$page_id = sanitize_text_field( $_POST['ets_tutor_lms_page_id'] );
+		if ( isset( $page_id ) ) {
+			$formated_discord_redirect_url = ets_get_tutor_lms_discord_formated_discord_redirect_url( $page_id );
+			update_option( 'ets_tutor_lms_discord_redirect_page_id', $page_id );
+			update_option( 'ets_tutor_lms_discord_redirect_url', $formated_discord_redirect_url );
+			$res = array(
+				'formated_discord_redirect_url' => $formated_discord_redirect_url,
+			);
+			wp_send_json( $res );
+
+		}
+		exit();
 
 	}
 
