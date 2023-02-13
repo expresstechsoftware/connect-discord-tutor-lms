c0b915dc21eb2758cb8114e2d72727e3cf369c8c
Younes DRO <dro66_8@hotmail.fr>
2023-02-13 15:16:56 +0100

    Remove tutor_load_template_after action hook
    Issue : it display the discord connect button in all template

--- includes/class-connect-discord-tutor-lms.php
+++ includes/class-connect-discord-tutor-lms.php
@@ -189,8 +189,6 @@ class Connect_Discord_Tutor_Lms {
 		$this->loader->add_filter( 'kses_allowed_protocols', $plugin_public, 'ets_tutor_lms_discord_allow_data_protocol' );
 		/** If load from page dashboard */
 		$this->loader->add_action( 'tutor_load_dashboard_template_before', $plugin_public, 'ets_tutor_lms_display_discord_button' );
-		/** If load from template */
-		$this->loader->add_action( 'tutor_load_template_after', $plugin_public, 'ets_tutor_lms_display_discord_button' );
 
 	}
 
